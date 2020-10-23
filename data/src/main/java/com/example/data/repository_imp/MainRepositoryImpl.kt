bd5f771dc7787f5d4d47ca92c43dd6a300292b64
ChangikChoe <46737716+ckddlr0610@users.noreply.github.com>
2020-10-23 16:26:46 +0900

    각 레이어 모듈 추가

---
+++ data/src/main/java/com/example/data/repository_imp/MainRepositoryImpl.kt
@@ -0,0 +1,5 @@
+package com.example.data.repository_imp
+
+class MainRepositoryImpl {
+
+}
---
+++ domain/src/main/java/com/example/domain/repository/MainRepository.java
@@ -0,0 +1,6 @@
+package com.example.domain.repository;
+
+// 레포지토리의 interface를 정의합니다.
+// implementation은 data layer에서 이뤄집니다.
+public interface MainRepository {
+}
